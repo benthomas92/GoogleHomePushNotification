ea95d65b297e3edad6d646f4ecbe1ed55ee715b5
Ben <bmtben@gmail.com>
2018-10-03 12:50:33 +1000

    First commit

---
+++ sendNotification.js
@@ -0,0 +1,27 @@
+var googlehomenotifier = require('@bmtben/google_home_notifier')("192.168.0.9", "en-US", 1)
+var express = require('express')
+var ngrok = require('ngrok')
+var bodyParser = require('body-parser')
+var app = express()
+
+const serverPort = 8091
+var savedConfig = {}
+app.use(express.static('public'))
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+
+app.post('/alexa-reminders', urlencodedParser, function (req, res) {
+  var reminder = req.body.reminder;
+  var date = req.body.datetime;
+  googlehomenotifier.notify(reminder, function (result) {
+    console.log(result);
+  })
+})
+
+app.listen(serverPort, function () {
+  ngrok.connect(serverPort, function (err, url) {
+    console.log('External endpoint: ' + url)
+    console.log('Internal endpoint: http://localhost:' + serverPort)
+  })
+  console.log('Google Notification app listening on port 8091')
+})
+
